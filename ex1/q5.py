853b342253c2f4af6737fc90bb4422191ed6c238
Jan Kruska <ema-kr@ISF.RWTH-AACHEN.DE>
2020-11-13 11:14:17 +0100

    Initial setup of first assignment

---
+++ ex1/q5.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+
+def main():
+    pos = np.arange(-5, 5.0, 0.1)# Returns a 100 dimensional vector <- It doesn't actually it creates a 1d 100-element vector
+
+def kde(samples, h):
+    # compute density estimation from samples with KDE
+    # Input
+    # samples    : DxN matrix of data points
+    # h          : (half) window size/radius of kernel
+    # Output
+    # estDensity : estimated density in the range of [-5,5]
+    return estDensity
+
+def knn(samples, k):
+    # compute density estimation from samples with KNN
+    # Input
+    # samples    : DxN matrix of data points
+    # k          : number of neighbors
+    # Output
+    # estDensity : estimated density in the range of [-5, 5]
+    return estDensity
+
+if __name__ == "__main__":
+    main()
