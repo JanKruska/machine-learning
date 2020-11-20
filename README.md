5390f28f9960d8f398192affb2bcc5560e09be9c
Jan Kruska <JanKruska@protonmail.com>
2020-11-20 19:57:33 +0100

    Loglikelihood

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # machine-learning
 Repository for submissions for the Machine-Learning lecture Prof. Bastian Leibe (WS20/21)
+
+### Produced using
+* Python 3.8.5
+* numpy 1.19.2
--- ex1/q6_expectation_maximization_python/getLogLikelihood.py
+++ ex1/q6_expectation_maximization_python/getLogLikelihood.py
@@ -14,6 +14,13 @@ def getLogLikelihood(means, weights, covariances, X):
     # OUTPUT:
     # logLikelihood  : log-likelihood
 
-    #####Insert your code here for subtask 6a#####
-    return logLikelihood
+    return np.sum([np.log(gauss_mix(x, means, weights, covariances)) for x in X])
+
+def gauss_mix(x, means, weights, covariances):
+    return np.sum([weights[k]*gaussian(x,means[k],covariances[:,:,k]) for k in range(len(weights))])
+
+def gaussian(x,mu,sigma):
+    d = sigma.shape[0]
+    factor = 1 / np.sqrt((np.power(2 * np.pi, d) * np.linalg.det(sigma)))
+    return factor * np.exp((-1 / 2 * np.dot((x-mu),np.dot(np.linalg.inv(sigma),(x-mu)))))
 
