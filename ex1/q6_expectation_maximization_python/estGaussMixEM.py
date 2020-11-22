91983fa92f54b21b07b7ac67d6839569906adfc3
Sagar Kalburgi <71485336+sagarkalburgi@users.noreply.github.com>
2020-11-22 21:36:36 +0530

    Add files via upload

--- ex1/q6_expectation_maximization_python/estGaussMixEM.py
+++ ex1/q6_expectation_maximization_python/estGaussMixEM.py
@@ -19,10 +19,11 @@ def estGaussMixEM(data, K, n_iters, epsilon):
     # covariances    : covariancesariance matrices of gaussians
     # logLikelihood  : log-likelihood of the data given the model
 
+    #####Insert your code here for subtask 6e#####
     n_dim = data.shape[1]
-    weights = np.ones(K) / K
+    weights = np.ones(K)/K
 
-    kmeans = KMeans(n_clusters=K,n_init=10).fit(data)
+    kmeans = KMeans(n_clusters=K, n_init=10).fit(data)
     cluster_idx = kmeans.labels_
     means = kmeans.cluster_centers_
 
@@ -31,9 +32,13 @@ def estGaussMixEM(data, K, n_iters, epsilon):
         data_cluster = data[cluster_idx==j]
         min_dist = np.inf
         for i in range(K):
-            #compute sum of distances
             dist = np.mean(euclidean_distances(data_cluster,[means[j]],squared=True))
-            if dist < min_dist:
-                min_dist = dist
+            if dist<min_dist:
+                min_dist=dist
         covariances[:,:,j] = np.eye(n_dim) * min_dist
+
+    for i in range(n_iters):
+        _, gamma = EStep(means, covariances, weights, data)
+        weights, means, covariances, _ = MStep(gamma, data)
+        #covariances = regularize_cov(covariances[:,:,i], epsilon)
     return [weights, means, covariances]
