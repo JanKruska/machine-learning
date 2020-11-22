4090a4dfe916e56b411edbf63ab56f288f665dee
Jan Kruska <JanKruska@protonmail.com>
2020-11-22 14:44:50 +0100

    Init values for estGaussMixEM.py

--- ex1/q6_expectation_maximization_python/estGaussMixEM.py
+++ ex1/q6_expectation_maximization_python/estGaussMixEM.py
@@ -19,5 +19,21 @@ def estGaussMixEM(data, K, n_iters, epsilon):
     # covariances    : covariancesariance matrices of gaussians
     # logLikelihood  : log-likelihood of the data given the model
 
-    #####Insert your code here for subtask 6e#####
+    n_dim = data.shape[1]
+    weights = np.ones(K) / K
+
+    kmeans = KMeans(n_clusters=K,n_init=10).fit(data)
+    cluster_idx = kmeans.labels_
+    means = kmeans.cluster_centers_
+
+    covariances = np.empty([n_dim,n_dim,K])
+    for j in range(K):
+        data_cluster = data[cluster_idx==j]
+        min_dist = np.inf
+        for i in range(K):
+            #compute sum of distances
+            dist = np.mean(euclidean_distances(data_cluster,[means[j]],squared=True))
+            if dist < min_dist:
+                min_dist = dist
+        covariances[:,:,j] = np.eye(n_dim) * min_dist
     return [weights, means, covariances]
