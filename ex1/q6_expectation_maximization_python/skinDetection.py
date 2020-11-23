44b16a1b2ab06a539973d8cbd73cb1fb88afd403
Sagar Kalburgi <71485336+sagarkalburgi@users.noreply.github.com>
2020-11-23 15:47:59 +0530

    further changes required

--- ex1/q6_expectation_maximization_python/skinDetection.py
+++ ex1/q6_expectation_maximization_python/skinDetection.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib import pyplot as plt
 from estGaussMixEM import estGaussMixEM
 from getLogLikelihood import getLogLikelihood
 
@@ -19,4 +20,18 @@ def skinDetection(ndata, sdata, K, n_iter, epsilon, theta, img):
     # result        : Result of the detector for every image pixel
 
     #####Insert your code here for subtask 1g#####
+    n_weights, n_means, n_covariances = estGaussMixEM(ndata, K, n_iter, epsilon)
+    s_weights, s_means, s_covariances = estGaussMixEM(sdata, K, n_iter, epsilon)
+    #print(img.shape[0])
+    #print(n_loglikelihood/s_loglikelihood)
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            n_loglikelihood = getLogLikelihood(n_weights, n_means, n_covariances, img[i][j])
+            s_loglikelihood = getLogLikelihood(s_weights, s_means, s_covariances, img[i][j])
+            #print(s_loglikelihood / n_loglikelihood)
+            if s_loglikelihood/n_loglikelihood > theta:
+                img[i][j] = 255
+            else:
+                img[i][j] = 0
+    result = img
     return result
