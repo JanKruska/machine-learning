28bba58996e32353813c5f0820ee4c838d1e4cf8
Sagar Kalburgi <71485336+sagarkalburgi@users.noreply.github.com>
2020-12-05 15:29:09 +0530

    Add files via upload

--- ex2/q2_svm_python/svmkern.py
+++ ex2/q2_svm_python/svmkern.py
@@ -1,6 +1,6 @@
 import numpy as np
 from kern import kern
-import cvxopt
+from cvxopt import matrix, solvers
 
 
 def svmkern(X, t, C, p):
@@ -18,4 +18,24 @@ def svmkern(X, t, C, p):
     # slack    : points inside the margin (boolean) (1 x num_samples)
 
     #####Insert your code here for subtask 2d#####
+    n = X.shape[0]
+    K = np.matmul(X, X.T)
+    P = matrix(np.matmul(t[:, np.newaxis], t[np.newaxis, :]) * K)
+    q = matrix(np.ones((n, 1)) * -1)
+    A = matrix(t.reshape(1, -1))
+    b = matrix(np.zeros(1))
+
+    G = matrix(np.vstack([np.eye(n) * -1, np.eye(n)]))
+    h = matrix(np.hstack([np.zeros(n), kern(C, np.ones(n), p)]))
+
+    solution = solvers.qp(P, q, G, h, A, b)
+    alpha = np.squeeze(solution['x'])
+
+    sv = alpha > 5 * 1e-4
+    w = np.dot(alpha * t, X)
+    # b = np.mean(t[sv] - np.diagonal(np.dot(alpha[sv]*t[sv]*X,X[sv].T)))
+    b = np.mean(t[sv] - np.sum(np.dot(X[sv], X[sv].T) * alpha[sv] * t[sv], axis=1))
+    # b = 0.00
+    result = np.sum(np.matmul(X[sv], X[sv].T) * alpha[sv] * t[sv], axis=0) + b
+    slack = np.array([])
     return alpha, sv, b, result, slack
